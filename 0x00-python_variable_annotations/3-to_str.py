8cc790ae616a9dcfeae4d8261952225f8a858640
Reagan <mamesareagan0@gmail.com>
2024-01-11 14:24:22 +0300

    to str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""converts a float to string"""
+
+
+def to_str(n: float) -> str:
+    """from float to string
+
+    Args:
+        n (float): float
+
+    Returns:
+        str: string
+    """
+    return str(n)
+
