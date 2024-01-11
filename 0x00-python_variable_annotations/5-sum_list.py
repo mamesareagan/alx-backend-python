d88f3eb6c07343953cd4e9c65057e149852db4e7
Reagan <mamesareagan0@gmail.com>
2024-01-11 14:38:05 +0300

    made necessary importations

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """sumation of floats"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """gets the sum of floats in a list
 
     Args:
@@ -11,4 +12,8 @@ def sum_list(input_list: list[float]) -> float:
     Returns:
         float: sum
     """
-    return sum(input_list)
+    if input_list is None:
+        return 0
+    else:
+         return sum(input_list)
+    
