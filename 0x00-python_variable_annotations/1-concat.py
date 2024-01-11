2229c95d14c2a6b45486565758f29d6160fd4da8
Reagan <mamesareagan0@gmail.com>
2024-01-11 13:36:09 +0300

    concatenation of two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""string concatenation_
+    """
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings
+
+    Args:
+        str1 (str): first string
+        str2 (str): second string
+    Returns:
+        str: _description_
+    """
+    return str1 + str2
