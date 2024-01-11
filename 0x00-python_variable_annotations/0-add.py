1698461a133e1d741e508ea1679733d36d188289
Reagan <mamesareagan0@gmail.com>
2024-01-11 13:13:20 +0300

    add function using float annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """adds two floats
+
+    Args:
+        a (float): 1st arg
+        b (float): 2nd arg
+
+    Returns:
+        float: return type
+    """
+    return a + b
