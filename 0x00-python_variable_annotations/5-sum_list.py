34791ed7a26237a7db8b1bf537a2ec002fbba5dc
Reagan <mamesareagan0@gmail.com>
2024-01-11 14:35:22 +0300

    sum of floats in a list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""sumation of floats"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """gets the sum of floats in a list
+
+    Args:
+        input_list (list[float]): list of floats
+
+    Returns:
+        float: sum
+    """
+    return sum(input_list)
